277dfc028af095ef94bfb354b2fe29a47f73e7d6
Fadil <fadilde@gmail.com>
2025-07-09 10:55:15 +0700

    Hello World Laravel project

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hello Laravel</title>
+</head>
+<body>
+    <h1 style="text-align:center; margin-top:100px;">
+        Hello World from Laravel, Fadil 🎉
+    </h1>
+</body>
+</html>
